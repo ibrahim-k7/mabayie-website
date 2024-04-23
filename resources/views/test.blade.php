214e69a73007e0f96739618192b57df5e0fdf939
ibrahim Mohammed <kxibrahimxk@gmail.com>
2024-04-24 00:12:14 +0300

    add admin template

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('title')
+    test page title
+@endsection
+
+@section('contentHeader')
+    this is content header
+@endsection
+
+@section("contentHeaderLink")
+    الرابط الاول
+@endsection
+
+@section("contentHeaderActive")
+<a href="#">الرابط الثاني</a>
+@endsection
+
+@section('content')
+    dd
+@endsection
